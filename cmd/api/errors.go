cc31fd2da1be086870e3dbbb57b8a9512be0cc7e
javierpoduje <javier.poduje@gmail.com>
2025-07-13 11:43:09 +0200

    ch 4.5: validating JSON input

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -48,3 +48,11 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
+
+func (app *application) failedValidationResponse(
+	w http.ResponseWriter,
+	r *http.Request,
+	errors map[string]string,
+) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
