15a20aeef0b760b9e42b6e6b887a1c7faf7fcadb
javierpoduje <javier.poduje@gmail.com>
2025-07-19 15:39:17 +0200

    ch 16.5: granting permissions

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"slices"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
@@ -17,7 +19,7 @@ type PermissionModel struct {
 	DB *sql.DB
 }
 
-// `GetAllForUser` method returns all permission codes for a specific user in a
+// GetAllForUser method returns all permission codes for a specific user in a
 // Permissions slice. The code in this method should feel very familiar
 func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	query := `
@@ -54,4 +56,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+// AddForUser adds the provided permission codes for a specific user.
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
